8c2ffc9ad97c2b7e43f57b3b459d01ecdf184aca
Ruza <vasilijevic.ruza.bps.com>
2022-10-31 19:04:15 +0100

    Initial commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,35 +1,35 @@
-import Head from "next/head";
-import Image from "next/image";
-import { useState } from "react";
-import ProductSlider from "../components/layout/ProductSlider";
-import SliderBanners from "../components/layout/SliderBanners";
-import styles from "../styles/Home.module.scss";
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+import ProductSlider from '../components/layout/ProductSlider';
+import SliderBanners from '../components/layout/SliderBanners';
+import styles from '../styles/Home.module.scss';
 
 //data
-import products from "../data/products.json";
-import HomeBanner from "../components/layout/HomeBanner/HomeBanner";
+import products from '../data/products.json';
+import HomeBanner from '../components/layout/HomeBanner/HomeBanner';
 
 const banners = [
   {
-    subtitle: "We celebrate",
-    title: "New in.",
-    button: "Explore more.",
-    image: "/images/banners/hp-banner-1.jpg",
+    subtitle: 'We celebrate',
+    title: 'New in.',
+    button: 'Explore more.',
+    image: '/images/banners/hp-banner-1.jpg',
     subButton: "Summer '22",
   },
   {
-    subtitle: "We celebrate2",
-    title: "New in.2",
-    button: "Explore more2",
-    image: "/images/banners/hp-banner-2.jpg",
-    subButton: "New collection",
+    subtitle: 'We celebrate2',
+    title: 'New in.2',
+    button: 'Explore more2',
+    image: '/images/banners/hp-banner-2.jpg',
+    subButton: 'New collection',
   },
   {
-    subtitle: "",
-    title: "",
-    button: "Explore more3",
-    image: "/images/banners/hp-banner-3.jpg",
-    subButton: "Bedding",
+    subtitle: '',
+    title: '',
+    button: 'Explore more3',
+    image: '/images/banners/hp-banner-3.jpg',
+    subButton: 'Bedding',
   },
 ];
 
